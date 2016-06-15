74ce4c204e4a81c885eed2f32c66cb50ebbacb2a
sujands23 <sujands23@gmail.com>
2016-06-15 07:03:20 -0500

    Adding Profile DataBase in DaataBase class

--- RESTfulMessengerApp/src/main/java/org/RESTfulMessengerApp/DataBase/DataBaseClass.java
+++ RESTfulMessengerApp/src/main/java/org/RESTfulMessengerApp/DataBase/DataBaseClass.java
@@ -6,8 +6,8 @@ import org.RESTfulMessengerApp.Model.*;
 
 public class DataBaseClass {
 
-	private static Map<Long, Message> messages=new HashMap<>();//Static because every message instance access the same Map
-	private static Map<String, Profile> profiles=new HashMap<>();//Static because every profile instance access the same Map
+	private static Map<Long, Message> messages=new HashMap<>(); //Static because every message instance access the same Map
+	private static Map<String, Profile> profiles=new HashMap<>(); //Static because every profile instance access the same Map
 	
 	public static Map<Long, Message> getMessages(){
 		return messages;
